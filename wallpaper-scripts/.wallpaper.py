ddbb7e04116c2877c4674bacf565f33caa98aab8
rharish101 <harish.rajagopals@gmail.com>
2019-09-12 20:12:57 +0530

    Cleaned code for wallpaper changer
    Added functionality for all other wallpaper styles.
    Subprocess no longer uses `shell=True`.
    Changed commandline arguments.
    Added docstrings and comments.

--- wallpaper-scripts/.wallpaper.py
+++ wallpaper-scripts/.wallpaper.py
@@ -1,292 +1,309 @@
 #!/usr/bin/python3
 """Script to do smooth wallpaper transitions in Xfce4."""
-import argparse
-import atexit
-import os
-import random
-import signal
-import subprocess
-import time
-from multiprocessing.dummy import Pool as ThreadPool
-import PIL
 from PIL import Image
 from Xlib.display import Display
+from Xlib.ext import randr
+from random import choice
+import os
+import subprocess
+from time import sleep
+from atexit import register
+from signal import signal, SIGTERM, SIGCONT, SIGSTOP
+from multiprocessing.dummy import Pool  # use threads instead of processes
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
 
 class WallpaperTransition:
+    """Class for performing wallpaper transition using Xfce."""
+
     def __init__(
-        self, imageFolder, blurFolder, timeout, duration, fps, backupPic
+        self,
+        image_folder,
+        blur_folder,
+        timeout,
+        duration,
+        fps,
+        backup_pic=None,
     ):
-        self.monitors = self.getMonitorList()
-        self.monitorNames = self.getMonitorNames()
-        self.imageFolder = imageFolder
-        self.blurFolder = blurFolder
+        """Initialize the instance.
+
+        Args:
+            image_folder (str): Path to the folder containing the wallpapers
+            blur_folder (str): Path to the folder containing the blurred
+                wallpapers
+            timeout (int): The wait between two transitions
+            duration (int): The duration of a transition
+            fps (int): The FPS for a transition
+            backup_pic (str): Path to a picture that will be applied when this
+                script crashes
+
+        """
+        self.image_folder = image_folder
+        self.blur_folder = blur_folder
         self.timeout = timeout
         self.duration = duration
         self.fps = fps
 
-        if backupPic:
-            self.backupPic = backupPic
-            atexit.register(self.backup)
-            signal.signal(signal.SIGTERM, self.signal_handler)
+        if backup_pic is not None:
+            self.backup_pic = backup_pic
+            self.backup = register(self.backup)
+            # `self.backup` will automatically be called when `exit` is called
+            signal(SIGTERM, lambda s, f: exit())
 
-    def getMonitorNames(self):
-        proc = subprocess.Popen(
-            ["xrandr", "--listmonitors"], stdout=subprocess.PIPE
-        )
-        out, err = proc.communicate()
+    def get_monitors(self):
+        """Get information on the connected monitors.
 
-        names = {}
-        for line in out.decode("utf8").strip().split("\n")[1:]:
-            monitor_num = line.strip().split(":")[0]
-            names[monitor_num] = line.strip().split()[-1]
-        return names
+        Returns:
+            dict: The keys are the monitor names (eg. "eDP1") and the values
+                are tuples of the monitor's resolution
 
-    def getMonitorList(self):
+        """
         display = Display()
         root = display.screen().root
         root.create_gc()
         resources = root.xrandr_get_screen_resources()._data
 
         monitors = {}
-        count = 0
 
         for crtc in resources["crtcs"]:
             monitor = display.xrandr_get_crtc_info(
                 crtc, resources["config_timestamp"]
             )._data
-            if len(monitor["outputs"]) > 0:  # is connected
-                monitors[count] = (monitor["width"], monitor["height"])
-                count += 1
+            for output in monitor["outputs"]:  # is connected
+                info = randr.get_output_info(
+                    root, output, resources["config_timestamp"]
+                )._data
+                monitors[info["name"]] = (monitor["width"], monitor["height"])
 
         return monitors
 
-    def getWallpaper(self, monitorNumber):
-        return (
-            subprocess.check_output(
-                "xfconf-query --channel xfce4-desktop "
-                "--property /backdrop/screen0/monitor"
-                + self.monitorNames[str(monitorNumber)]
-                + "/workspace0/last-image",
-                shell=True,
-            )
-            .decode("utf-8")
-            .replace("\n", "")
-        )
+    def get_wallpaper(self, monitor_id):
+        """Get the path to the current wallpaper of the given monitor."""
+        cmd = [
+            "xfconf-query",
+            "--channel",
+            "xfce4-desktop",
+            "--property",
+            f"/backdrop/screen0/monitor{monitor_id}/workspace0/last-image",
+        ]
+        return subprocess.check_output(cmd).decode("utf-8").strip()
 
-    def setWallpaper(self, monitorNumber, imagePath):
+    def set_wallpaper(self, monitor_id, image_path):
+        """Set the current wallpaper for the given monitor."""
         subprocess.call(
-            "xfconf-query --channel xfce4-desktop --property "
-            "/backdrop/screen0/monitor"
-            + self.monitorNames[str(monitorNumber)]
-            + "/workspace0/last-image --set "
-            + imagePath,
-            shell=True,
+            [
+                "xfconf-query",
+                "--channel",
+                "xfce4-desktop",
+                "--property",
+                f"/backdrop/screen0/monitor{monitor_id}/workspace0/last-image",
+                "--set",
+                image_path,
+            ]
         )
 
-    def getWallStyle(self, monitorNumber):
-        return int(
-            subprocess.check_output(
-                "xfconf-query --channel xfce4-desktop "
-                "--property /backdrop/screen0/monitor"
-                + self.monitorNames[str(monitorNumber)]
-                + "/workspace0/image"
-                "-style",
-                shell=True,
-            )
-            .decode("utf-8")
-            .replace("\n", "")
-        )
+    def get_wall_style(self, monitor_id):
+        """Get the style of the current wallpaper of the given monitor.
 
-    def processImage(self, img, size, wall_style=None):
-        if wall_style == 5:
-            img_size = img.size
-            if (float(img_size[0]) / img_size[1]) > (float(size[0]) / size[1]):
-                img = img.resize(
-                    tuple(
-                        int((float(size[1]) / img_size[1]) * i)
-                        for i in img.size
-                    ),
-                    PIL.Image.ANTIALIAS,
-                )
-                img = img.crop(
-                    (
-                        (img.size[0] - size[0]) / 2,
-                        0,
-                        (img.size[0] + size[0]) / 2,
-                        img.size[1],
-                    )
-                )
-            elif (float(img_size[0]) / img_size[1]) < (
-                float(size[0]) / size[1]
-            ):
-                img = img.resize(
-                    tuple(
-                        int((float(size[0]) / img_size[0]) * i)
-                        for i in img.size
-                    ),
-                    PIL.Image.ANTIALIAS,
-                )
-                img = img.crop(
-                    (
-                        0,
-                        (img.size[1] - size[1]) / 2,
-                        img.size[0],
-                        (img.size[1] + size[1]) / 2,
-                    )
-                )
+        The style is one of:
+            Centered: Image un-resized, placed at the center
+            Tiled: Image un-resized, tiled from top-left to bottom-right
+            Stretched: Image resized with change of aspect ratio to the screen
+            Scaled: Image downsized (keeping aspect ratio constant) to fit
+                within the screen, with a black background
+            Zoomed: Image resized (keeping aspect ratio constant) to fit over
+                the screen, and the extra cropped out
+
+        """
+        cmd = [
+            "xfconf-query",
+            "--channel",
+            "xfce4-desktop",
+            "--property",
+            f"/backdrop/screen0/monitor{monitor_id}/workspace0/image-style",
+        ]
+        return int(subprocess.check_output(cmd).decode("utf-8").strip())
+
+    def process_image(self, img, target_size, wall_style=None):
+        """Modify the given image to the given size according to the wallpaper style.
+
+        The style is one of:
+            Centered: Image un-resized, placed at the center
+            Tiled: Image un-resized, tiled from top-left to bottom-right
+            Stretched: Image resized with change of aspect ratio to the screen
+            Scaled: Image downsized (keeping aspect ratio constant) to fit
+                within the screen, with a black background
+            Zoomed: Image resized (keeping aspect ratio constant) to fit over
+                the screen, and the extra cropped out
+
+        """
+        if wall_style == 5:  # Zoomed
+            # Preserve aspect ratio by resizing and then center-cropping
+            if (img.size[0] / img.size[1]) > (target_size[0] / target_size[1]):
+                upscale_ratio = target_size[1] / img.size[1]
             else:
-                img = img.resize(size, PIL.Image.ANTIALIAS)
-        elif wall_style == 3:
-            img = img.resize(size, PIL.Image.ANTIALIAS)
-        else:
-            img.thumbnail(size, PIL.Image.ANTIALIAS)
-        img.convert("RGBA")
-
-        offset_x = int(max((size[0] - img.size[0]) / 2, 0))
-        offset_y = int(max((size[1] - img.size[1]) / 2, 0))
-        offset_tuple = (offset_x, offset_y)
-
-        result = Image.new(mode="RGB", size=size, color=(0, 0, 0, 0))
-        result.paste(img, offset_tuple)
-        return result
-
-    def bgTransition(self, monitorID, imageFolder):
-        current = self.getWallpaper(monitorID)
-        new = imageFolder + "/" + random.choice(os.listdir(imageFolder))
-
-        bg = self.processImage(
-            Image.open(current),
-            self.monitors[monitorID],
-            self.getWallStyle(monitorID),
-        )
-        fg = self.processImage(
-            Image.open(new),
-            self.monitors[monitorID],
-            self.getWallStyle(monitorID),
-        )
-
-        count = (self.duration * self.fps) + 1
-        sleep = self.duration / self.fps
-
-        for i in range(1, count):
-            Image.blend(bg, fg, i / count).save(
-                "/tmp/" + str(monitorID) + "_" + str(i) + ".jpg"
+                upscale_ratio = target_size[0] / img.size[0]
+            img = img.resize(
+                tuple(int(upscale_ratio * dim) for dim in img.size),
+                Image.ANTIALIAS,
             )
-        for i in range(1, count):
-            time.sleep(sleep)
-            self.setWallpaper(
-                monitorID, "/tmp/" + str(monitorID) + "_" + str(i) + ".jpg"
+            img = img.crop(
+                (
+                    (img.size[0] - target_size[0]) // 2,
+                    (img.size[1] - target_size[1]) // 2,
+                    (img.size[0] + target_size[0]) // 2,
+                    (img.size[1] + target_size[1]) // 2,
+                )
             )
-        self.setWallpaper(monitorID, new)
-        subprocess.call(
-            "rm /tmp/" + str(monitorID) + "_*.jpg", shell=True
-        )  # can't use wildcards in pythons os.remove()
 
-    def signal_handler(self, signal, frame):
-        self.backup()
+        elif wall_style == 3:  # Stretched
+            # Aspect ratio is not preserved
+            img = img.resize(target_size, Image.ANTIALIAS)
+
+        elif wall_style == 2:  # Tiled
+            bg = Image.new(mode="RGBA", size=target_size, color=(0, 0, 0, 0))
+            for offset_x in range(0, target_size[0], img.size[0]):
+                for offset_y in range(0, target_size[1], img.size[1]):
+                    bg.paste(img, (offset_x, offset_y))
+            img = bg
+
+        else:  # Centered or Scaled
+            if wall_style == 4:  # Scaled
+                # `thumbnail` preserves the aspect ratio, by downsizing the
+                # largest dimension to fit within the given size
+                img.thumbnail(target_size, Image.ANTIALIAS)
+
+            bg = Image.new(mode="RGBA", size=target_size, color=(0, 0, 0, 255))
+            offset_x = int((target_size[0] - img.size[0]) / 2)
+            offset_y = int((target_size[1] - img.size[1]) / 2)
+            bg.paste(img, (offset_x, offset_y))
+            img = bg
+
+        # Alpha channel is required as this image might be merged with another
+        # RGBA image
+        img.convert("RGB")
+        return img
+
+    def bg_transition(self, monitor_id, image_folder):
+        """Perform a transition into a randomly chosen wallpaper."""
+        current = self.get_wallpaper(monitor_id)
+        available = os.listdir(image_folder)
+        # Avoid changing into itself
+        available.remove(os.path.basename(current))
+        new = os.path.join(image_folder, choice(available))
+
+        wall_style = self.get_wall_style(monitor_id)
+        bg = self.process_image(
+            Image.open(current), self.monitors[monitor_id], wall_style
+        )
+        fg = self.process_image(
+            Image.open(new), self.monitors[monitor_id], wall_style
+        )
+
+        total_imgs = int(self.duration * self.fps)
+        wait = self.duration / self.fps
+        TMP_FMT = "/tmp/{}_{}.jpg"
+
+        for i in range(1, total_imgs + 1):
+            Image.blend(bg, fg, i / total_imgs).save(
+                TMP_FMT.format(monitor_id, i)
+            )
+
+        # FPS and duration is used for this
+        for i in range(1, total_imgs + 1):
+            sleep(wait)
+            self.set_wallpaper(monitor_id, TMP_FMT.format(monitor_id, i))
+        self.set_wallpaper(monitor_id, new)
+
+        for i in range(1, total_imgs + 1):
+            os.remove(TMP_FMT.format(monitor_id, i))
 
     def backup(self):
-        for id in self.monitors:
-            self.setWallpaper(id, self.backupPic)
+        """Set the backup image on all connected monitors."""
+        # Connected monitors may have changed
+        for monitor_id in self.get_monitors():
+            self.set_wallpaper(monitor_id, self.backup_pic)
 
     def loop(self):
-        while 1:
-            pgrep = subprocess.Popen(
-                ["pgrep", "blur-desktop"], stdout=subprocess.PIPE
-            )
-            pid_list = list(map(int, pgrep.stdout.read().strip().split()))
+        """Loop and perform transitions."""
+        while True:
+            pgrep = subprocess.check_output(["pgrep", "blur-desktop"])
+            pid_list = map(int, pgrep.strip().split())
 
-            if len(pid_list) > 0:
-                for pid in pid_list:
-                    os.kill(pid, 19)
+            for pid in pid_list:
+                os.kill(pid, SIGSTOP.value)
 
             array = []
-            self.monitors = self.getMonitorList()
-            for id in self.monitors:
-                curr = self.getWallpaper(id)
-                if self.blurFolder is not None and self.blurFolder in curr:
-                    array.append((id, self.blurFolder))
+            self.monitors = self.get_monitors()
+            for monitor_id in self.monitors:
+                curr = self.get_wallpaper(monitor_id)
+                if self.blur_folder is not None and curr in os.listdir(
+                    self.blur_folder
+                ):
+                    array.append((monitor_id, self.blur_folder))
                 else:
-                    array.append((id, self.imageFolder))
+                    array.append((monitor_id, self.image_folder))
 
-            pool = ThreadPool(len(self.monitors))
-            pool.starmap(self.bgTransition, array)
+            with Pool(len(self.monitors)) as pool:
+                pool.starmap(self.bg_transition, array)
 
-            if len(pid_list) > 0:
-                for pid in pid_list:
-                    os.kill(pid, 18)
+            for pid in pid_list:
+                os.kill(pid, SIGCONT.value)  # send SIGCONT
 
-            time.sleep(self.timeout)
+            sleep(self.timeout)
+
+
+def main(args):
+    """Run the main program.
+
+    Arguments:
+        args (`argparse.Namespace`): The object containing the commandline
+            arguments
+
+    """
+    wt = WallpaperTransition(
+        args.img_dir,
+        args.blur_dir,
+        args.timeout,
+        args.duration,
+        args.fps,
+        backup_pic=args.backup,
+    )
+    wt.loop()
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="XFCE Wallpaper Transition")
-    parser.add_argument(
-        "-d",
-        "--dir",
-        # default=os.getcwd(),
-        default="/home/rharish/Pictures/Wallpapers",
-        metavar=("ImageDir"),
-        help="The directory of the backgrounds you want to loop through",
-        type=str,
+    parser = ArgumentParser(
+        description="XFCE Wallpaper Transition",
+        formatter_class=ArgumentDefaultsHelpFormatter,
     )
     parser.add_argument(
-        "-D",
-        "--blur-dir",
-        # default=os.getcwd(),
-        default="/home/rharish/.blurred_wallpapers/Level_3",
-        metavar=("BlurDir"),
-        help="The directory of the blurred backgrounds",
+        "img_dir",
+        metavar="ImgDir",
         type=str,
+        help="the directory of the backgrounds you want to loop through",
     )
     parser.add_argument(
-        "-t",
+        "--blur-dir", type=str, help="the directory of the blurred backgrounds"
+    )
+    parser.add_argument(
         "--timeout",
+        type=int,
         default=600,
-        metavar=("SECONDS"),
-        help="How many seconds to wait before the next transition",
-        type=int,
+        help="idle period (in seconds) between transitions",
     )
     parser.add_argument(
-        "-s",
-        "--transition",
-        nargs=2,
-        default=[1, 60],
-        metavar=("DURATION FPS"),
-        help="Defines how long a transition is and with how much FPS it "
-        "shall be done",
-        type=int,
+        "--duration",
+        type=float,
+        default=1.0,
+        help="duration of one transition (in seconds)",
     )
     parser.add_argument(
-        "-b",
+        "--fps", type=int, default=60, help="FPS for the transition"
+    )
+    parser.add_argument(
         "--backup",
-        default="/home/rharish/Pictures/Wallpapers/photo-1433785124354.jpg",
-        metavar=("BackupPicture"),
-        help="The backup picture to revert to, if the program crashes",
         type=str,
+        help="the backup picture to revert to, if the program crashes",
     )
-
-    args = vars(parser.parse_args())
-
-    if "dir" in args:
-        dir = args["dir"]
-    if "blur_dir" in args:
-        blur_dir = args["blur_dir"]
-    else:
-        blur_dir = None
-    if "timeout" in args:
-        timeout = args["timeout"]
-    if "transition" in args:
-        duration = args["transition"][0]
-        fps = args["transition"][1]
-    if "backup" in args:
-        backupPic = args["backup"]
-
-    if dir and blur_dir and timeout and duration and fps:
-        wt = WallpaperTransition(
-            dir, blur_dir, timeout, duration, fps, backupPic
-        )
-        wt.loop()
-    else:
-        parser.print_help()
+    main(parser.parse_args())
