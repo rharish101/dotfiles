ae89c6ba2d24dd3c104dff95276e776e1d2bdf74
rharish101 <harish.rajagopals@gmail.com>
2020-01-25 14:06:33 +0530

    Cleaned code

--- i3lock/.i3locker.sh
+++ i3lock/.i3locker.sh
@@ -1,63 +1,73 @@
-#!/usr/bin/zsh
-# Do getopt stuff
-# https://stackoverflow.com/a/29754866/7905483
+#!/usr/bin/bash
+read -r -d '' usage <<EOF
+Usage: `basename $0` [-h] [-d LockedDpms] [-i Image]
+Use i3lock-color to lock the screen with multi-monitor support
 
-! getopt --test > /dev/null
-if [[ ${pipestatus[1]} -ne 4 ]]; then
-    echo "I’m sorry, `getopt --test` failed in this environment."
-    exit 1
-fi
+    -h, --help        Display help and exit
+    -d LockedDpms, --dpms LockedDpms
+                      DPMS settings when locked (default: $dpms_locked)
+    -i Image, --image Image
+                      Background image (default: $image)"
+EOF
 
-OPTIONS=d:i:h
-LONGOPTS=dpms:,image:,help
+ARGS=(
+    "--nofork"
+    "--ignore-empty-password"
+    "--pointer=default"
+    "--composite"
+    "--indicator"
+    "--radius=25"
+    "--insidecolor=383C4A00"
+    "--insidevercolor=383C4A00"
+    "--insidewrongcolor=ED071700"
+    "--line-uses-inside"
+    "--ring-width=6"
+    "--ringcolor=D3DAE388"
+    "--ringvercolor=D3DAE333"
+    "--verifcolor=D3DAE300"
+    "--veriftext=''"
+    "--ringwrongcolor=ED0717AA"
+    "--wrongcolor=D3DAE300"
+    "--wrongtext=''"
+    "--keyhlcolor=D3DAE3FF"
+    "--bshlcolor=ED0717AA"
+    "--separatorcolor=1A162800"
+    "--indpos=350:1010"
+    "--force-clock"
+    "--timecolor=D3DAE3FF"
+    "--timestr=%H:%M"
+    "--timesize=32"
+    "--time-align=1"
+    "--timepos=40:1005"
+    "--time-font=roboto-light"
+    "--datecolor=D3DAE3FF"
+    "--datestr='Type password to unlock'"
+    "--datesize=20"
+    "--date-align=1"
+    "--datepos=40:1037"
+    "--date-font=roboto-light"
+)
 
-# -use ! and pipestatus to get exit code with errexit set
-# -temporarily store output to be able to check for errors
-# -activate quoting/enhanced mode (e.g. by writing out “--options”)
-# -pass arguments only via   -- "$@"   to separate them correctly
-! PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
-if [[ ${pipestatus[1]} -ne 0 ]]; then
-    # e.g. return value is 1
-    #  then getopt has complained about wrong arguments to stdout
-    echo $usage
-    exit 2
-fi
-# read getopt’s output this way to handle the quoting right:
-eval set -- "$PARSED"
-
-do_help=false
-disable_help=false
+dpms_unlocked="$(xset q | awk '/Standby/{print $2, $4, $6}')"
 dpms_locked="10 10 10"
+num_monitors=$(xrandr --query | grep " connected" | wc -l)
 image="$HOME/.local/share/backgrounds/i3locker-background.png"
 
-usage="Usage: `basename $0` [-h] [-d LockedDpms] [-i Image]
-    \rUse i3lock-color to lock the screen with multi-monitor support
-
-    \r    -h, --help        Display help and exit
-    \r    -d LockedDpms, --dpms LockedDpms
-    \r                      DPMS settings when locked (default: $dpms_locked)
-    \r    -i Image, --image Image
-    \r                      Background image (default: $image)"
-
-# now enjoy the options in order and nicely split until we see --
-while true; do
+while [ ! -z "$1" ]; do
     case "$1" in
         -h|--help)
-            do_help=true
-            shift
+            echo "$usage"
+            exit 0
             ;;
         -d|--dpms)
             dpms_locked="$2"
-            disable_help=true
-            shift 2
+            shift
             ;;
         -i|--image)
             image="$2"
-            disable_help=true
-            shift 2
+            shift
             ;;
         --)
-            shift
             break
             ;;
         *)
@@ -65,24 +75,8 @@ while true; do
             exit 3
             ;;
     esac
+    shift
 done
-
-# handle non-option arguments
-if [[ $# -ne 0 ]]; then
-    echo $usage
-    exit -1
-fi
-
-if [ $do_help = true ]; then
-    echo $usage
-    if [ $disable_help = true ]; then
-        exit -1
-    else
-        exit 0
-    fi
-fi
-
-# Script starts!!!
 
 dpms_revert()
 {
@@ -93,31 +87,32 @@ trap dpms_revert HUP INT TERM
 dpms_set()
 {
     while true; do
-        dpms_stat=$(xset q | grep "Standby" | awk '{print $2, $4, $6}')
-        if [[ $dpms_stat != $dpms_locked ]]; then
-            sh -c "xset dpms $dpms_locked"
+        dpms_curr="$(xset q | awk '/Standby/{print $2, $4, $6}')"
+        if [[ "$dpms_curr" != "$dpms_locked" ]]; then
+            sh -c "xset dpms $dpms_locked"  # zsh doesn't split $dpms_locked
         fi
         sleep 5
     done
 }
-# xautolock -enable
-
-dpms_unlocked=$(xset q | grep "Standby" | awk '{print $2, $4, $6}')
-num_monitors=`xrandr --query | grep " connected" | wc -l`
 
 # Check if eog is installed; eog is used with compiz for fade-in/fade-out
 which eog > /dev/null || eog=:
 
-eog -f -g $image --class="i3lock" &
+eog -f -g "$image" --class="i3lock" &
+eog_pids="$!"
 if [[ $num_monitors == 2 ]]
 then
-    eog -n -f -g $image --class="i3lock-1" &
+    eog -n -f -g "$image" --class="i3lock-1" &
+    eog_pids="$eog_pids $!"
 fi
 
 sleep 0.5
 dpms_set &
-# /home/rharish/.multi-lock -i $image -a "-n -e -p default --composite --indicator -k --radius 25 --insidecolor=383C4A00 --insidevercolor=383C4A00 --insidewrongcolor=BE384100 --line-uses-inside --ring-width=6 --ringcolor=D3DAE3AA --ringvercolor=D3DAE355  --verifcolor=D3DAE300 --veriftext=\"\" --ringwrongcolor=BE3841AA --wrongcolor=D3DAE300 --wrongtext=\"\" --keyhlcolor=1A1628FF --bshlcolor=BE3841AA --separatorcolor=1A162800 --indpos=\"350:1010\" --force-clock --timecolor=D3DAE3AA --timestr=\"%H:%M\" --timesize=32 --time-align=1 --timepos=\"40:1005\" --time-font=roboto-light --datecolor=D3DAE3AA --datestr=\"Type password to unlock\" --datesize=20 --date-align=1 --datepos=\"40:1037\" --date-font=roboto-light"
-$HOME/.multi-lock -i $image -a "-n -e -p default --composite --indicator -k --radius 25 --insidecolor=383C4A00 --insidevercolor=383C4A00 --insidewrongcolor=ED071700 --line-uses-inside --ring-width=6 --ringcolor=D3DAE388 --ringvercolor=D3DAE333  --verifcolor=D3DAE300 --veriftext=\"\" --ringwrongcolor=ED0717AA --wrongcolor=D3DAE300 --wrongtext=\"\" --keyhlcolor=D3DAE3FF --bshlcolor=ED0717AA --separatorcolor=1A162800 --indpos=\"350:1010\" --force-clock --timecolor=D3DAE3FF --timestr=\"%H:%M\" --timesize=32 --time-align=1 --timepos=\"40:1005\" --time-font=roboto-light --datecolor=D3DAE3FF --datestr=\"Type password to unlock\" --datesize=20 --date-align=1 --datepos=\"40:1037\" --date-font=roboto-light"
-killall eog
+dpms_pid=$!
+
+# $HOME/.multi-lock -i "$image" -a "$(IFS=' '; printf '%s' "${ARGS[*]}")"
+i3lock -i "$image" "${ARGS[@]}"
+
+sh -c "kill $eog_pids"  # zsh doesn't expand $eog_pids
+kill $dpms_pid
 dpms_revert
-pkill -P $$
