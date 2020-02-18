9a9dbf668aa65965127f5d0739b81a184eeac50d
rharish101 <harish.rajagopals@gmail.com>
2020-02-18 13:26:59 +0530

    Adjusted XAVA window size
    Cleaned the Devilspie2 config for XAVA.
    Removed XAVA's setting for window alignment as it has no effect.

--- xava/.config/devilspie2/XAVA.lua
+++ xava/.config/devilspie2/XAVA.lua
@@ -1,10 +1,19 @@
 #!/usr/bin/env lua
+OFFSET = 3;  -- for hiding XAVA's empty bars
+LAPTOP_WIDTH = 1920;  -- for shifting to the second monitor
+PANEL_HEIGHT = 48;  -- to avoid XAVA overlapping with Xfce's panel
+
 if (get_window_class() == "XAVA") then
-    set_window_type("_NET_WM_WINDOW_TYPE_DOCK");
-    if (get_window_name() == "XAVA") then
-        set_window_position(333, 940);
-    elseif (get_window_name() == "XAVA1") then
-        --set_window_position(2221, 735); --for 1280x1024
-        set_window_position(1920 + 333, 940); --for 1920x1080
+    set_window_type("_NET_WM_WINDOW_TYPE_DOCK")
+
+    local screen_width, screen_height = get_screen_geometry()
+    local _, _, win_width, win_height = get_window_geometry()
+    local x_pos = (screen_width - win_width) // 2
+    local y_pos = screen_height - PANEL_HEIGHT - win_height + OFFSET
+
+    if (get_window_name() == "XAVA") then -- for the laptop's screen
+        set_window_position(x_pos, y_pos)
+    elseif (get_window_name() == "XAVA1") then -- for the second monitor
+        set_window_position(LAPTOP_WIDTH + x_pos, y_pos)
     end
 end
