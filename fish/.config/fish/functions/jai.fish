af0825ca05303b4640bf40a17b62bfcef4afea7c
Harish Rajagopal <harish.rajagopals@gmail.com>
2026-08-13 20:42:42 +0200

    [jai] Tighten included dirs
    - Only include OpenCode config dir from dotfiles as read-only.
    - Only include git worktree root & .git dir, drop others.

--- fish/.config/fish/functions/jai.fish
+++ fish/.config/fish/functions/jai.fish
@@ -7,39 +7,12 @@ function jai --wraps jai --description 'jai wrapper to include git roots'
         return 1
     end
 
-    set dirs
-    if git rev-parse --is-inside-work-tree &>/dev/null
-        if test "$(git rev-parse --is-inside-work-tree)" = 'false'
-            # Bare repo
-            set root (git rev-parse --git-common-dir | path dirname)
-        else
-            set root (git rev-parse --show-toplevel)
-        end
-        set --append dirs $root
-
-        # Check for worktree
-        if test -f "$root/.git"
-            set parent (git rev-parse --git-common-dir | path dirname)
-            if test -d "$parent/.git"
-                set --append dirs $parent
-            end
-        end
-
-        # Check for submodule
-        set superproject (git rev-parse --show-superproject-working-tree 2>/dev/null)
-        if test -n "$superproject"
-            set --append dirs $superproject
-        end
-    end
-
     set args
-    for d in $dirs
-        if test "$d" = '.'
-            set d (pwd)
+    if git rev-parse --is-inside-work-tree &>/dev/null
+        if test "$(git rev-parse --is-inside-work-tree)" = 'true'
+            set --append args --dir (git rev-parse --show-toplevel)
         end
-        if test "$(path normalize $d)" != "$(path normalize (pwd))"
-            set --append args --dir $d
-        end
+        set --append args --dir (git rev-parse --git-common-dir)
     end
 
     command jai $args $argv
